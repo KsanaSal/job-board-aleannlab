6b70836d97ba2338512348961a3033ee76886d94
KsanaSal <oasalivon@gmail.com>
2022-11-11 13:13:16 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-3xl font-bold text-red-500">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
