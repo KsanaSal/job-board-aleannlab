6c44b09cb41ac087bf32354f103e743d8d63e639
KsanaSal <oasalivon@gmail.com>
2022-11-16 12:33:05 +0200

    Styled header

--- src/pages/JobDetails.tsx
+++ src/pages/JobDetails.tsx
@@ -10,12 +10,14 @@ const JobDetails = ({ jobs }: { jobs: Job[] }) => {
     console.log(jobs.filter((data) => data.id === id));
     console.log(job);
     return (
-        <div className="container mx-auto rounded-lg bg-white py-6 px-4 md:py-14 grid">
-            <div>
-                <header className=" grid ">
-                    <h1 className="font-bold text-tx28 tracking-wide text-dark">
-                        Job Details
-                    </h1>
+        <div className="container mx-auto rounded-lg bg-white py-6 px-4 md:py-14 my-5 grid">
+            <div className="md:min-w-fit">
+                <header className=" grid md:grid-cols-2 md:border-b-2 md:pb-2">
+                    <div className="border-b-2 mb-6 md:border-b-0 md:mb-0">
+                        <h1 className="font-bold text-tx28 tracking-wide text-dark mb-3 md:mb-0">
+                            Job Details
+                        </h1>
+                    </div>
                     <div className="flex gap-9 md:gap-6">
                         <div className="flex gap-3 md:gap-2 items-center">
                             <BookmarkIcon className="hidden md:block w-8" />
